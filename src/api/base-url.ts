ba9d1b64a6d026e4066b7d54b050f7c98a04299a
supremozee <supremozee14@gmail.com>
2025-03-06 23:15:42 +0100

    Update env

--- src/api/base-url.ts
+++ src/api/base-url.ts
@@ -1,1 +1,1 @@
-export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL_PRODUCTION || "https://api-simplip2p.onrender.com"
+export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL_PRODUCTION || "https://backend-19ny.onrender.com"
