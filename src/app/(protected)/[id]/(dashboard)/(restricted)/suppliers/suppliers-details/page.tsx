432d501d4e58555dcd632c761cd55a8a76ca3b6c
supremozee <supremozee14@gmail.com>
2025-05-22 21:21:21 +0100

    fix

--- src/app/(protected)/[id]/(dashboard)/(restricted)/suppliers/suppliers-details/page.tsx
+++ src/app/(protected)/[id]/(dashboard)/(restricted)/suppliers/suppliers-details/page.tsx
@@ -1,10 +1,10 @@
-import SupplierDetails from '@/components/organisms/SupplierDetails'
-import React from 'react'
+// import SupplierDetails from '@/components/organisms/SupplierDetails'
+// import React from 'react'
 
-const page = () => {
-  return (
-    <SupplierDetails/>
-  )
-}
+// const page = () => {
+//   return (
+//     <SupplierDetails/>
+//   )
+// }
 
-export default page
+// export default page
