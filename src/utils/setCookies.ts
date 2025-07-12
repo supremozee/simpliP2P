4117272ade44585d2959414090593377a91d423b
supremozee <supremozee14@gmail.com>
2025-07-12 11:25:05 +0100

    fix

--- src/utils/setCookies.ts
+++ src/utils/setCookies.ts
@@ -14,7 +14,7 @@ const getDomain = () => {
 export const setCookies = (accessToken: string, refreshToken: string) => {
   const cookieOptions = {
     path: "/",
-    secure: process.env.NODE_ENV === "production",
+    secure: false,
     sameSite: "lax" as const,
     domain:getDomain()
   };
